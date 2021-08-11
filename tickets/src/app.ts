26a83a44ddd9b98d0b7100afd1e41b75c5268fab
Fabiano Sbaratta <fabiano.sbaratta@gmail.com>
2021-08-11 16:52:05 +0200

    Added tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';       //importing express framework
+import 'express-async-errors';
+import { json } from 'body-parser';  //using json middleware to parse incoming req with json payloads
+import cookieSession from 'cookie-session'; // we are going to store a JWT inside a cookie to realize (and transport) the authentication mechanism
+
+import { NotFoundError,errorHandler,currentUser } from '@s1lv3rf0x/common';
+
+import { createTicketRouter } from './routes/new';
+
+const app = express();
+app.set('trust proxy',true); //to make sure that express understands that is behind a proxy of Ingress nginx and still trust the traffic as secure
+app.use(json());
+app.use(
+    cookieSession({
+        //we are disabling encryption because JWT will already do that
+        signed: false,
+        //cookie will only be used if a user is visiting the application on a https connection (secure: true)
+        secure: process.env.NODE_ENV !== 'test'  //this way we set to false if we are in a test env
+    })
+);
+app.use(currentUser);
+//Routes
+app.use(createTicketRouter);
+
+app.all('*', async (req,res) => {
+    throw new NotFoundError();
+});
+//Error handler middleware
+app.use(errorHandler);
+//
+
+export { app };
---
+++ tickets/src/routes/new.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+import { currentUser, requireAuth } from '@s1lv3rf0x/common';
+
+const router = express.Router();
+
+router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+export { router as createTicketRouter };
